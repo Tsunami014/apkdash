73f7c1b84195732b6508e111a854c86ec39c1daa
Tsunami014 <tsunami014@duck.com>
2025-12-22 08:34:10 +1100

    Improved some window stuff!

--- gui/base.py
+++ gui/base.py
@@ -1,8 +1,35 @@
-from abc import ABC, abstractmethod
+from .displ import fix, printScreen
+import builtins
 
-class Window(ABC):
-    __slots__ = ['buf']
+class Window:
+    __slots__ = ['buf', 'sidebuf']
 
-    @abstractmethod
-    def update(self): ...
+    def __init__(self):
+        self.buf = ""
+        self.sidebuf = ""
+
+    def _bufprt(self, *args, sep=" ", end="\n"):
+        self.buf += sep.join(args)+end
+    def _sideprt(self, *args, sep=" ", end="\n"):
+        self.sidebuf += sep.join(args)+end
+
+    def update(self):
+        _oldprt = builtins.print
+        builtins.print = self._bufprt
+        self._upd()
+        builtins.print = self._sideprt
+        self._updSide()
+        builtins.print = _oldprt
+        self.buf = fix(self.buf)
+        self.sidebuf = fix(self.sidebuf)
+
+    def _upd(self): pass
+    def _updSide(self): pass
+
+    def updprint(self):
+        self.update()
+        self.print()
+
+    def print(self):
+        printScreen(self)
 
--- gui/displ.py
+++ gui/displ.py
@@ -1,12 +1,14 @@
 """
 \\020
 
-< go to start of this line (use with 'end="\\020<"')
+< Clear last line
+!! Clear screen
 
 + good
 - bad
 * warn
 ~ info
+
 b bold
 i invert
 r reset colour
@@ -25,10 +27,15 @@ cB colour Black (invisible)
 import shutil
 import re
 
+def fix(txt):
+    idx = txt.rfind("\020!!")
+    if idx != -1:
+        txt = txt[idx+3:]
+    return re.sub('^.*\n?.*\020<', '', txt).strip("\n")
+
 def toPrintable(txt):
     safe = re.sub('\033\\[[0-9;]+.', '', txt)
     return safe\
-        .replace('\020<', '\r')\
         .replace('\020+', '[\03392m+\033[39m] ')\
         .replace('\020-', '[\03394m-\033[39m] ')\
         .replace('\020*', '[\03393m*\033[39m] ')\
@@ -45,7 +52,8 @@ def toPrintable(txt):
         .replace('\020cy', '\033[93m')\
         .replace('\020cW', '\033[97m')\
         .replace('\020cG', '\033[90m')\
-        .replace('\020cB', '\033[30m')
+        .replace('\020cB', '\033[30m')\
+        .replace('\020', '�')
 
 def popBuf(buf: str, wid: int):
     idx = buf.find("\n")
@@ -53,21 +61,29 @@ def popBuf(buf: str, wid: int):
         return buf[:wid].ljust(wid), buf[wid:]
     return buf[:idx].ljust(wid), buf[idx+1:]
 
-def printScreen(buf, sidebuf=None):
+def printScreen(wind):
     size = shutil.get_terminal_size()
-    if sidebuf is None:
+    buf = toPrintable(wind.buf)
+    mxidx = 0
+    if not wind.sidebuf:
         print("╭"+"─"*(size.columns-2)+"╮")
-        for _ in range(size.lines-2):
+        for i in range(size.lines-2):
             prt, buf = popBuf(buf, size.columns-2)
+            if buf.strip(" "):
+                mxidx = i
             print("│"+prt+"│")
         print("╰"+"─"*(size.columns-2)+"╯", end="\033[0;0H", flush=True)
     else:
+        sidebuf = toPrintable(wind.sidebuf)
         wid1 = (size.columns-3)//3
         wid2 = (size.columns-3)-wid1
         print("╭"+"─"*wid1+"┬"+"─"*wid2+"╮")
         for _ in range(size.lines-2):
             prt1, sidebuf = popBuf(sidebuf, wid2)
             prt2, buf = popBuf(buf, wid2)
+            if buf.strip(" ") or sidebuf.strip(" "):
+                mxidx = i
             print("│"+prt1+"│"+prt2+"│")
         print("╰"+"─"*wid1+"┴"+"─"*wid2+"╯", end="\033[0;0H", flush=True)
+    return mxidx
 
